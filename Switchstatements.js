adf2cdee9c0ee3259a2d089c9b917f4f272962c5
Praveenmaila <164868094+Praveenmaila@users.noreply.github.com>
2025-04-23 22:44:10 +0530

    Create Switchstatements.js

---
+++ Switchstatements.js
@@ -0,0 +1,12 @@
+let day = "Monday";
+
+switch(day) {
+  case "Monday":
+    console.log("Start of the week");
+    break;
+  case "Friday":
+    console.log("Weekend loading...");
+    break;
+  default:
+    console.log("Just another day");
+}
