a1b05af9010455acae5d7ea0572e404968a367cf
Praveenmaila <164868094+Praveenmaila@users.noreply.github.com>
2025-04-26 22:00:34 +0530

    Add files via upload

---
+++ Operators/Assignment.js
@@ -0,0 +1,6 @@
+let num = 30; // num=30 
+let num += 10; // num=num+10 => num=40 
+let num -= 10; // num=num-10 => num=20 
+let num *= 30; // num=num*30 => num=900 
+let num /= 10; // num=num/10 => num=3 
+let num %= 10; // num=num%10 => num=0 
---
+++ Operators/typeofop.js
@@ -0,0 +1,6 @@
+typeof "JavaScript World" //string 
+typeof 10.5 // number 
+typeof 10 > 20 //boolean 
+typeof undefined //undefined 
+typeof null //Object 
+typeof {itemPrice : 500} //Object 
